4fcde211dea8427f4ae2b1997e3143cf06473ca1
tcdmv <bamchaar@gmail.com>
2019-07-05 10:39:23 -0400

    added a testchrome to lounchchrome maven project

--- louchChrome/src/main/java/louchChrome/chromebrow.java
+++ louchChrome/src/main/java/louchChrome/chromebrow.java
@@ -1,21 +1,24 @@
 package louchChrome;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class chromebrow {
 static WebDriver driver  ;
+static String x;
 	public static void main(String[] args) {
 		
-		chromeinstance();
+		chromeinstance(x);
 	}
-	public static void chromeinstance() {
+	public static void chromeinstance(String x) {
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Abdel\\Documents\\Cloned Servers\\maven1\\louchChrome\\lib\\Drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https:\\www.google.com");
-		
+		driver.findElement(By.name("q")).sendKeys(x);
+		driver.findElement(By.name("btnI")).click();
 	}
 
 }
---
+++ louchChrome/src/test/java/louchChrome/testchromebrow.java
@@ -0,0 +1,15 @@
+package louchChrome;
+
+import org.junit.Test;
+
+public class testchromebrow {
+
+	@Test
+	public void testchrome() {
+		
+		chromebrow nc = new chromebrow();
+		String x = "Abdel";
+		nc.chromeinstance(x);
+		
+	}
+}
